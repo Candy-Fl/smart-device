fc2112b6e22580b7cfe9e42103e23acb83a5a93f
candyy <alekseykim01@gmail.com>
2022-05-28 08:38:08 +0700

    First

--- source/js/utils/popup.js
+++ source/js/utils/popup.js
@@ -3,6 +3,7 @@ function popup() {
   const callBtn = document.querySelector('.page__header-call-btn');
   const popupContainer = document.querySelector('.popup');
   const sendBtn = document.querySelector('.popup__send-button');
+  const closeBtn = document.querySelector('.popup__close');
 
   function sendData(data) {
     fetch('https://echo.htmlacademy.ru/', {
@@ -12,10 +13,17 @@ function popup() {
     );
   }
 
+  closeBtn.addEventListener('click', () => {
+    popupContainer.classList.add('visually-hidden');
+    body.classList.remove('scroll-lock');
+  });
+
   callBtn.addEventListener('click', () => {
     window.scrollTo(0, 0);
-    popupContainer.classList.remove('visually-hidden');
-    body.classList.add('scroll-lock');
+    setTimeout(() => {
+      popupContainer.classList.remove('visually-hidden');
+      body.classList.add('scroll-lock');
+    }, (500));
   });
 
   sendBtn.addEventListener('click', () => {
